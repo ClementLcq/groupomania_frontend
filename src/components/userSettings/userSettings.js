84128ba9f5101755a3958a2bc286f9f3ccab258d
Clement Lecocq <hello@clementlecocq.com>
2022-09-20 10:43:00 +0200

    (USERsetting) Ajout date du jour

--- src/components/userSettings/userSettings.js
+++ src/components/userSettings/userSettings.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const UserSettings = () => {
+    const current = new Date();
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
     return (
         <section className='userSettings'>
-            <p>Nom de l'utilisateur</p>
-            <p>Paramètres</p>
+            <p>Bonjour Nom de l'utilisateur,</p>
+            <p>nous sommes le {date}.</p>
         </section>
     );
 };
