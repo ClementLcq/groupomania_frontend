68eda41d1cd43946a7fc5c83b56f4517bda05bdf
Clement Lecocq <hello@clementlecocq.com>
2022-09-22 12:06:38 +0200

    (POST/DisplayPost) Essai recup datas api

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,28 +1,82 @@
 import React from 'react';
 import LikeButton from './utils/likeButton';
+// import {useEffect, useState} from "react";
+// import axios from "axios";
 
-const Post = () => {
-    return (
-            <article className="post">
-                <div className='post__header'>
-                    <h3 className='post__header__author'>Auteur du post</h3>
-                    <div className="post__header__date"> 11:01 06/09/2022</div>
-                </div>
-                <img src="./payet.jpg" alt="" className='post__image' />
-                <p className='post__description'>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed orci rhoncus, ornare ipsum ut, congue augue. Sed ornare urna leo, non dapibus tortor porttitor accumsan. Quisque volutpat scelerisque nisl sit amet luctus. Nullam laoreet hendrerit mi feugiat porttitor. Ut id lobortis mi. Aliquam ipsum nisl, placerat ac velit in, consequat congue orci. Duis metus eros, pharetra at metus et, lacinia finibus ex.</p>
-                <div className='post__features'>
-                    <div className="post__features__opinion">
-                        <LikeButton />
-                    </div>
-                    <div className="post__features__editing">
-                        <button className='editButton'>Modifier</button>
-                        <button className='deleteButton'>Supprimer</button>
-                    </div>
-                </div>
-            </article>
+const Post = (props) => {
+
+    const {posts} = props;
+
+    if(posts.length > 0) { 
+
+        return (
+            posts.map((post) => {
+                console.log(post)
+                return (
+                    <article className="post" key={post._id}>
+                        <div className='post__header'>
+                            <h3 className='post__header__author'>{post.userId}</h3>
+                            <div className="post__header__date"> 11:01 06/09/2022</div>
+                        </div>
+                        <img src="" alt="" className='post__image' key={post.imageUrl}/>
+                        <p className='post__description'>
+                        {post.description}
+                        </p>
+                        <div className='post__features'>
+                            <div className="post__features__opinion">
+                                <LikeButton key={post.likes} />
+                            </div>
+                            <div className="post__features__editing">
+                                <button className='editButton'>Modifier</button>
+                                <button className='deleteButton'>Supprimer</button>
+                            </div>
+                        </div>
+                    </article>
+                )
+            })
+        )} else {
+            return (
+                <article className="post">
+                    <p className='post__description'>
+                        Pas de post pour le moment
+                    </p>
+                </article>
+            )
+        }
+}
+
+// const Post = () => {
+//     const url = 'https://localhost:3001/api/posts';
+//     const [post, setPost] = useState([]);
+
+//     useEffect(() => {
+//         axios.get(url).then(res => {
+//         setPost(res.data.post);
+//         })
+//     }, [])
+  
+//     return (
+//             <article className="post">
+//                 <div className='post__header'>
+//                     <h3 className='post__header__author'>Auteur du post</h3>
+//                     <div className="post__header__date"> 11:01 06/09/2022</div>
+//                 </div>
+//                 <img src="./payet.jpg" alt="" className='post__image' />
+//                 <p className='post__description'>
+//                 {post.description}
+//                 </p>
+//                 <div className='post__features'>
+//                     <div className="post__features__opinion">
+//                         <LikeButton />
+//                     </div>
+//                     <div className="post__features__editing">
+//                         <button className='editButton'>Modifier</button>
+//                         <button className='deleteButton'>Supprimer</button>
+//                     </div>
+//                 </div>
+//             </article>
             
-    );
-};
+//     );
+// };
 
 export default Post;
--- src/components/Posts/displayPosts.js
+++ src/components/Posts/displayPosts.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Post from './Post';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-const DisplayPosts = (props) => {
+const DisplayPosts = () => {
+
+    const [posts, getPosts] = useState('');
+
+    const url = "http://localhost:3001/api";
+
+    useEffect(() => {
+        getAllPosts();
+    }, []);
+
+    const getAllPosts = () => {
+        axios.get(`${url}/posts`)
+        .then((res) => {
+            const allPosts = res.data.posts.allPosts;
+            getPosts(allPosts);
+        })
+        .catch((err) => console.error(err));
+    }
+
     return (
         <section className='displayPosts'>
-            <Post />  
-            <Post />
+            <Post posts={posts}/>  
         </section>
     );
 };
