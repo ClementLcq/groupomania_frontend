ff1d6cc54e10ebbcdfa6a5d29be3677fe001a525
Clement Lecocq <hello@clementlecocq.com>
2022-09-22 12:05:52 +0200

    RAS

--- src/components/userSettings/userSettings.js
+++ src/components/userSettings/userSettings.js
@@ -1,15 +1,34 @@
 import React from 'react';
+// import componentDidMount from 'react';
 
-const UserSettings = () => {
-    const current = new Date();
-    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+const current = new Date();
+const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
 
-    return (
-        <section className='userSettings'>
-            <p>Bonjour Nom de l'utilisateur,</p>
-            <p>nous sommes le {date}.</p>
-        </section>
-    );
-};
+const UserSettings = () =>{
+
+      return (
+            <section className='userSettings'>
+                <p>Bonjour Nom de l'utilisateur,</p>
+                <p>nous sommes le {date}.</p>
+            </section>
+   ) 
+  }
+
+// const UserSettings = () => {
+//     const current = new Date();
+//     const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
+//     componentDidMount() {
+//         fetch('http://localhost:3000/users')
+//         .then(res => res.json());
+//     }
+//     render() {
+//         return (
+//             <section className='userSettings'>
+//                 <p>Bonjour Nom de l'utilisateur,</p>
+//                 <p>nous sommes le {date}.</p>
+//             </section>
+//         );}
+// };
 
 export default UserSettings;
