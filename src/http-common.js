0d37b6ddcb3c621b6195a7bc8f538c5d72d2759b
Clement Lecocq <hello@clementlecocq.com>
2022-09-29 12:41:11 +0200

    (Structure) Essai postService

---
+++ src/http-common.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const token = localStorage.getItem("token");
+
+export default axios.create({
+  baseURL: "http://localhost:3001/api",
+  headers: {
+    "Content-type": "application/json",
+    "Authorization": `Bearer ${token}`
+  }
+});
