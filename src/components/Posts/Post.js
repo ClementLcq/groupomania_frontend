e53e41d9b0afb48228b5d4ab422a08a3ace0e998
Clement Lecocq <hello@clementlecocq.com>
2022-10-13 14:21:55 +0200

    (POSTS) Ajout display buttons modif/delete si bon userId

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // import DisplayPosts from './displayPosts';
 import LikeButton from './utils/likeButton';
+import ModifyButton from './utils/modifyButton';
+import DeleteButton from './utils/deleteButton';
 // import {useEffect, useState} from "react";
 // import axios from "axios";
 
@@ -9,30 +11,33 @@ const Post = (props) => {
     const {posts} = props;
     console.log(posts);
     
-
     if(posts.length > 0) { 
+
+        const currentUserId = JSON.parse(localStorage.getItem('userId'));
 
         return (
             posts.map((post, index) => {
                 return (
                     <article className="post" key={index}>
                         <div className='post__header'>
-                            <h3 className='post__header__author'>{post.User[0].email.split('@')[0].split('.').join(' ')}</h3>
+                            <h3 className='post__header__author'>{post.User[0].email}</h3>
                             <div className="post__header__date"> {post.createdAt.split('T').join(' ').split('.')[0]}</div>
                         </div>
                         <img src={post.imageUrl} alt="" className='post__image'/>
                         <p className='post__description'>
                         {post.description}
                         </p>
-                        <div className='post__features'>
-                            <div className="post__features__opinion">
-                                <LikeButton/>
+                        { currentUserId === post.userId ? (
+                            <div className='post__features'>
+                                <div className="post__features__opinion">
+                                    <LikeButton/>
+                                </div>
+                                <div className="post__features__editing">
+                                    <ModifyButton />
+                                    <DeleteButton />                          
+                                </div>
                             </div>
-                            <div className="post__features__editing">
-                                <button className='editButton'>Modifier</button>
-                                <button className='deleteButton'>Supprimer</button>
-                            </div>
-                        </div>
+                        ) : null}  
                     </article>
                 )
             })
--- src/components/userSettings/userSettings.js
+++ src/components/userSettings/userSettings.js
@@ -3,15 +3,15 @@ import React from 'react';
 const current = new Date();
 const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
 const userEmail = JSON.parse(localStorage.getItem("userEmail"));
-const emailName = userEmail.split('@');
-const userName = emailName[0].split('.').join(' ');
+// const emailName = userEmail.split('@');
+// const userName = emailName[0].split('.').join(' ');
 
 
 const UserSettings = () =>{
 
       return (
             <section className='userSettings'>
-                <p className='userSettings__hello'>Bonjour {userName},</p>
+                <p className='userSettings__hello'>Bonjour {userEmail},</p>
                 <p>nous sommes le {date}.</p>
             </section>
    ) 
