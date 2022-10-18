d133a1bcda5238181009fb74afb0266e65710e28
Clement Lecocq <hello@clementlecocq.com>
2022-10-18 16:56:39 +0200

    (LOGOUT) Mise en place de la fonction logout + style

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -4,11 +4,23 @@ import Logo from '../components/Layout/Logo';
 import NavigationLogOn from '../components/Layout/LogOn/navigation.LogOn';
 
 const LogOut = () => {
+
+    const handleLogOut = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userEmail');
+        localStorage.removeItem('userId');
+        window.location = "/";
+    }
+
     return (
         <div>
             <Logo />
             <NavigationLogOn />
-            Log Out
+            <div className='logout'>
+                <p className='logout__text' > Merci d'être venu sur notre réseau social.<br/> Veuillez cliquer sur le bouton pour vous déconnecter</p>
+                <button onClick={handleLogOut} className='logout__button'>Se déconnecter</button>
+            </div>
+            
             <Footer />
         </div>
     );
