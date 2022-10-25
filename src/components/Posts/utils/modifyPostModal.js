5afa6e470fb929b938b5ba68b6afd68862b8cedf
Clement Lecocq <hello@clementlecocq.com>
2022-10-25 19:48:42 +0200

    modify ok

--- src/components/Posts/utils/modifyPostModal.js
+++ src/components/Posts/utils/modifyPostModal.js
@@ -1,49 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { useEffect } from 'react';
 
 const ModifyPostModal = ({open, onClose, postId} ) => {
-
 
     const [post, setPost] = useState({});
     const [description, setDescription] = useState("");
     const [file, setFile] = useState(null);
-    // const [postImage, setPostImage] = useState();
 
     const token = localStorage.getItem("token");
 
-    // console.log(postId);
+    const getPost = async () => {
+        const currentPostId = postId;
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/posts/${currentPostId}`, {headers: {Authorization: `Bearer ${token}`}})
+        setPost(res.data);
+        setFile(res.data.image);
+        setDescription(res.data.description);
+    }
 
-    // const handleFile = (e) => {
-    //     setPostImage(URL.createObjectURL(e.target.value[0]));
-    //     setFile(e.target.file[0]);
-    // }
-
-    useEffect(() => {
-        const getPost = async () => {
-            const currentPostId = postId;
-            const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/posts/${currentPostId}`, {headers: {Authorization: `Bearer ${token}`}})
-            setPost(res.data);
-            setFile(res.data.image);
-            setDescription(res.data.description);
-        };
+    useEffect(() => {  
+        if(postId)
         getPost();
     }, [postId, token])
 
-    // useEffect(()=> {
-        // axios.get(`${url}/posts/${postId}`, {headers: {Authorization: `Bearer ${token}`}})
-    //     .then( res => {
-    //         const data = res.data;
-    //         // console.log(data);
-    //         setDescription(() => data.description)
-    //         setFile(() => data.imageUrl)
-
-    //     })
-    // }, [])
-
-    // récup de l'userId et userEmail dans le LS
     const userId = JSON.parse(localStorage.getItem('userId'));
-    const userEmail = JSON.parse(localStorage.getItem('userEmail'));
 
     const handleFormChange = async (e) => {
         const newFile = file ? file : "";
@@ -56,33 +35,13 @@ const ModifyPostModal = ({open, onClose, postId} ) => {
         window.location.reload();
     };
 
-    // const handleFormChange = async (e) => {
-    //     e.preventDefault();
-    //     if (description || file) {
-    //         const formData = new FormData();
-    //         formData.append("userId", userId);
-    //         formData.append("userEmail", userEmail);
-    //         formData.append("image", file);
-    //         formData.append("description", description);
-
-    //         console.log(formData)
-
-    //         await axios.put(`http://localhost:3001/api/posts/${postId}`, formData, {headers: {Authorization: `Bearer ${token}`}});
-    //         setDescription("");
-    //         setFile(null);
-    //         window.location.reload();
-    //     }
-        
-    // }
-
     if(!open) return null
     
     return (
         <div onClick={onClose} className='overlay'>
             <div onClick={(e) => e.stopPropagation()} className="modalContainer createPost__options">
                 <button onClick={onClose} className="closeButton">X</button>
-                {/* <form action="/posts" method="post" encType="multipart/form-data" className='modalContainer__form' onSubmit={handleFormChange}> */}
-                <form action="/posts" method="post" encType="multipart/form-data" className='modalContainer__form'>
+                <form className='modalContainer__form' onSubmit={handleFormChange}>
                         <textarea type="text"
                                 id="description"
                                 required
@@ -98,7 +57,7 @@ const ModifyPostModal = ({open, onClose, postId} ) => {
                                 Ajouter une image
                                 <p className="file-name"></p>
                             </label>
-                            <button className="createPost__options__submit" type="button" onClick={handleFormChange}>Mettre à jour l'article</button>   
+                            <input className="createPost__options__submit" type="submit"  value="Mettre à jour l'article"/>
                         </div>
                 </form>
             </div>
