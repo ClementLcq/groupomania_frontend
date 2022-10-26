c1bb4b1733bce4371a34cf2aa95ba1a24f08f9f8
Clement Lecocq <hello@clementlecocq.com>
2022-10-26 10:34:53 +0200

    ReactDom render to createRoot render

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
+import { createRoot } from 'react-dom/client';
 
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
         <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>);
